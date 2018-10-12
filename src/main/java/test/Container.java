926fd05eb5e5cd21d4951e4ec0a884fb36bd48a2
hjk <412815191@qq.com>
2018-10-12 11:19:25 +0800

    容器接口

--- src/main/java/test/Container.java
+++ src/main/java/test/Container.java
@@ -6,5 +6,5 @@ public interface Container<T> {
 	public boolean add(T t) throws Exception;//向容器添加元素
 	public T get() throws Exception;//获取容器元素
 	public int getSize() throws Exception;//获取容器大小
-	public List<T> getList() throws Exception;//获取容器
+	public List<T> getList() throws Exception;//获取容器的链表
 }
